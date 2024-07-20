bfd2cfa97204b502a5148a23dd1bd9c3539a33d4
kionahutch <155482290+kionahutch@users.noreply.github.com>
2024-07-20 12:07:03 -0400

    Update index.js
    Removing personal comments used for learning purposes to give a cleaner look.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const userRoutes = require('./techmates-backend/routes/RouteUser'); // Correct path
+const userRoutes = require('./techmates-backend/routes/RouteUser'); 
 console.log("Before DB connection");
-require('./techmates-backend/config/db'); // Import the database configuration
+require('./techmates-backend/config/db'); // database configuration
 
 const app = express();
 console.log("After DB connection");
 
-app.use(express.json()); // Middleware to parse JSON
+app.use(express.json()); // Middleware 
 console.log("JSON Middleware added");
 
-app.use('/api/users', userRoutes); // Use the user routes
+app.use('/api/users', userRoutes); 
 console.log("User routes added");
 
 const PORT = process.env.PORT || 5001;
